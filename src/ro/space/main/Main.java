efa17a456ed5368f0e7dbfb5b1285eedbf8bcb42
lucian <lucian.stefanoaica@gmail.com>
2015-05-15 18:18:47 +0300

    just tested the app to see if it works on linux (used ubuntu 12.04 - 32 bits) and it worked just fine

--- src/ro/space/main/Main.java
+++ src/ro/space/main/Main.java
@@ -3,6 +3,7 @@ package ro.space.main;
 
 public class Main {
   public static void main(String[] args) {
+	// Just a simple comment to test git...
     Application app = new Application();
     app.init();
     app.start();
